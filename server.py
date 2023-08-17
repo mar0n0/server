b3e00b0b52b2032a9bb112c4ff58919e7931f34b
mar0n0 <joaomarono@protonmail.com>
2023-08-17 16:51:15 +0100

    Server sending score and piis

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from quart import Quart, request, jsonify
 import asyncio
 import subprocess
 import os
+import json
 
 app = Quart(__name__)
 SCANNER_DIR = "../privacy_quantification"
@@ -22,8 +23,33 @@ async def scan_apk(package_name, package_version):
     cmd = "python3 {} -s {} -a {} -p {} -v {}".format(os.path.join(SCANNER_DIR, "scan.py"), SCANNER_DIR, APKS_DIR, package_name, package_version)
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
     out, err = p.communicate()
-    result = out.decode().split('\n')[0]
-    return result
+    #result = out.decode().split('\n')[0]
+    result = out.decode().split('\n')
+    methods_piis = result[0]
+    permissions_piis = result[1]
+    score = result[2]
+    return methods_piis, permissions_piis, score
+
+
+# Format PIIS information for response
+def formatPIIS(methods_piis, permissions_piis):
+    
+    piis = ""
+    methods_piis_json = methods_piis.replace("'", '"')
+    methods = json.loads(methods_piis_json)
+    
+    for method in methods:
+        piis = piis + method[0] + ":" + method[1] + ";"
+        
+    
+    permissions_piis_json = permissions_piis.replace("'", '"')
+    permissions = json.loads(permissions_piis_json)
+    
+    for permission in permissions:
+        piis = piis + permission[0] + ":" + permission[1] + ";"
+    
+    return piis
+
 
 
 @app.route('/analysis', methods=['POST'])
@@ -31,13 +57,22 @@ async def handle_request():
     data = await request.get_json()
     packagename = data['packagename']
     packageversion = data['packageversion']
-    
     print("Received a request for the APK: {} with the Version: {}".format(packagename, packageversion))
     
-    result = await download_apk(packagename, packageversion) # Download APK
+    # Download APK
+    result = await download_apk(packagename, packageversion) 
+    
     if result == "Success":
-        result = await scan_apk(packagename, packageversion) # Analyse APK
-    return jsonify({"score": result}) 
+        if os.path.exists(os.path.join(APKS_DIR, packagename, str(packageversion), packagename + ".apk")):
+            # Analyse APK
+            methods_piis, permissions_piis, score = await scan_apk(packagename, packageversion) 
+            piis = formatPIIS(methods_piis, permissions_piis)
+            print(piis)
+            return jsonify({"score": score, "piis": piis}) 
+        else:
+            return jsonify({"error": "Analysis tool did not find apk"})
+    else:
+        return jsonify({"error": "Error during apk download!"})
     
 
 if __name__ == '__main__':
